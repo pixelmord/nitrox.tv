ec91e6cad2985f1e169f51df921e9f018237b13d
Andreas Sahle <andreas.sahle@gmail.com>
2020-04-17 08:30:49 +0200

    fix: fallback for unset cover image

--- src/components/SEO/SEO.jsx
+++ src/components/SEO/SEO.jsx
@@ -18,7 +18,7 @@ class SEO extends Component {
       description = postMeta.description
         ? postMeta.description
         : postNode.excerpt;
-      image = postMeta.cover.childImageSharp.fluid.src;
+      image = postMeta.cover ? postMeta.cover.childImageSharp.fluid.src : '';
       postURL = urljoin(config.siteUrl, config.pathPrefix, postPath);
     } else {
       title = config.siteTitle;

f9b05607288f4403fbdfe743c68d81a1cea67714
Andreas Sahle <andreas.sahle@gmail.com>
2020-04-17 08:54:08 +0200

    feat: use correct site shortname for disqus

--- data/SiteConfig.js
+++ data/SiteConfig.js
@@ -10,7 +10,7 @@ const config = {
   siteRssTitle: "Nitrox.TV RSS feed", // Title of the RSS feed
   siteFBAppID: "1825356251115265", // FB Application ID for using app insights
   googleAnalyticsID: "UA-10770592-1", // GA tracking ID.
-  disqusShortname: "https-vagr9k-github-io-gatsby-advanced-starter", // Disqus shortname.
+  disqusShortname: "nitrox-tv", // Disqus shortname.
   dateFromFormat: "YYYY-MM-DD", // Date format used in the frontmatter.
   dateFormat: "DD.MM.YYYY", // Date format for display.
   postsPerPage: 25, // Amount of posts displayed per listing page.
